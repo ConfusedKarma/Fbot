b070c259debdbdff0384078572aeb928ec421e4c
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-04-02 21:11:24 +0530

    Update eval.py

--- plugins/eval.py
+++ plugins/eval.py
@@ -2,67 +2,19 @@ import io
 import sys
 import traceback
 from pyrogram import Client, filters
+from datetime import datetime
+from platform import python_version
+from pyrogram import __version__
 
 
 CUSTOM_CMD ="!"
+START_TIME = datetime.now()
 
-@Client.on_message(filters.command("eval", CUSTOM_CMD)
-async def eval(client, message):
-    status_message = await message.reply_text("Processing ...")
-    cmd = message.text.split(" ", maxsplit=1)[1]
-
-    reply_to_ = message
-    if message.reply_to_message:
-        reply_to_ = message.reply_to_message
-
-    old_stderr = sys.stderr
-    old_stdout = sys.stdout
-    redirected_output = sys.stdout = io.StringIO()
-    redirected_error = sys.stderr = io.StringIO()
-    stdout, stderr, exc = None, None, None
-
-    try:
-        await aexec(cmd, client, message)
-    except Exception:
-        exc = traceback.format_exc()
-
-    stdout = redirected_output.getvalue()
-    stderr = redirected_error.getvalue()
-    sys.stdout = old_stdout
-    sys.stderr = old_stderr
-
-    evaluation = ""
-    if exc:
-        evaluation = exc
-    elif stderr:
-        evaluation = stderr
-    elif stdout:
-        evaluation = stdout
-    else:
-        evaluation = "Success"
-
-    final_output = "<b>EVAL</b>: "
-    final_output += f"<code>{cmd}</code>\n\n"
-    final_output += "<b>OUTPUT</b>:\n"
-    final_output += f"<code>{evaluation.strip()}</code> \n"
-
-    if len(final_output) > 4096:
-        with io.BytesIO(str.encode(final_output)) as out_file:
-            out_file.name = "eval.text"
-            await reply_to_.reply_document(
-                document=out_file,
-                caption=cmd[: 4096 // 4 - 1],
-                disable_notification=True,
-                quote=True,
-            )
-    else:
-        await reply_to_.reply_text(final_output, quote=True)
-    await status_message.delete()
-
-
-async def aexec(code, client, message):
-    exec(
-        "async def __aexec(client, message): "
-        + "".join(f"\n {l_}" for l_ in code.split("\n"))
+@Client.on_message(filters.command("up", CMD_TRIGGER))
+async def up(_, message: Message):
+    txt = (
+        f"-> Current Uptime: `{str(datetime.now() - START_TIME).split('.')[0]}`\n"
+        f"-> Python: `{python_version()}`\n"
+        f"-> Pyrogram: `{__version__}`"
     )
-    return await locals()["__aexec"](client, message)
+    await message.reply(txt)
