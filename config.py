0fb581314b71527c61ea01783e79814d4be6ca72
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-04-02 15:31:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-      BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+      TOKEN = os.environ.get("BOT_TOKEN", "")
       APP_ID = int(os.environ.get("APP_ID", 12345))
       API_HASH = os.environ.get("API_HASH")
-      CHANNEL_ID = list(x for x in os.environ.get("CHANNEL_ID", "").replace("\n", " ").split(' '))
+      CHANNEL = list(x for x in os.environ.get("CHANNEL_ID", "").replace("\n", " ").split(' '))
