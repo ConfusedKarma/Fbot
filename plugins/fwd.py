cb444e3fd3d887313c43f73594984031f3b54333
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-04-03 09:51:37 +0530

    Update fwd.py

--- plugins/fwd.py
+++ plugins/fwd.py
@@ -4,6 +4,6 @@ from Fbot import fbot
 a = [594813047] # add user ids as list
 b = -1001193710741 # add the chat id of channel
 
-@fbot.on_message(filters.chats(a))
+@fbot.on_message(filters.chat(a))
 async def fwd(message):
     await message.forward(b)
