876c7e2f350ca9c1ac7a1d2ecd54fb1e80f92d0c
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-04-03 11:37:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,11 @@ class Config(object):
       APP_ID = int(os.environ.get("APP_ID", 12345))
       API_HASH = os.environ.get("API_HASH")
       CHANNEL = list(x for x in os.environ.get("CHANNEL_ID", "").replace("\n", " ").split(' '))
-      AUTH_USERS = list(os.environ.get("AUTH_USERS")
+
+AUTH_USERS = set()
+
+if os.path.exists('auth_users.txt'):
+    with open('auth_users.txt', 'r+') as f:
+        lines = f.readlines()
+        for line in lines:
+            AUTH_USERS.add(int(line.split()[0]))
