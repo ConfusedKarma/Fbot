22628f3ba171efe849624f562f415153bc8e8cf2
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-04-03 19:49:59 +0530

    Update and rename eval_exec.py to exec.py

---
+++ plugins/exec.py
@@ -0,0 +1,60 @@
+import asyncio
+import io
+import os
+import sys
+import traceback
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from Fbot import fbot
+
+CUSTOM_CMD = "!"
+
+
+def ReplyCheck(message: Message):
+    reply_id = None
+
+    if message.reply_to_message:
+        reply_id = message.reply_to_message.message_id
+
+    elif not message.from_user.is_self:
+        reply_id = message.message_id
+
+    return reply_id
+
+@Client.on_message(filters.command("exec", CUSTOM_CMD))
+async def execution(fbot, message: Message):
+    cmd = message.text.split(" ", maxsplit=1)[1]
+
+    reply_to_id = message.message_id
+    if message.reply_to_message:
+        reply_to_id = message.reply_to_message.message_id
+
+    process = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    e = stderr.decode()
+    if not e:
+        e = "No errors"
+    o = stdout.decode()
+    if not o:
+        o = "No output"
+
+    OUTPUT = ""
+    OUTPUT += f"<b>Command:</b>\n<code>{cmd}</code>\n\n"
+    OUTPUT += f"<b>Output</b>: \n<code>{o}</code>\n"
+    OUTPUT += f"<b>Errors</b>: \n<code>{e}</code>"
+
+    if len(OUTPUT) > 4096:
+        with open("exec.text", "w+", encoding="utf8") as out_file:
+            out_file.write(str(OUTPUT))
+        await message.reply_document(
+            document="exec.text",
+            caption=cmd,
+            disable_notification=True,
+            reply_to_message_id=ReplyCheck(message),
+        )
+        os.remove("exec.text")
+    else:
+        await message.reply(OUTPUT)
