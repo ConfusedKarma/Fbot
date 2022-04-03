c1e5ac77ea08b1ca0fd575d1f69ce31e8409bf16
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-04-03 12:35:25 +0530

    Update fwd.py

--- plugins/fwd.py
+++ plugins/fwd.py
@@ -1,11 +1,10 @@
-from pyrogram import filters
-from Fbot import fbot
-from config import Config
+#from pyrogram import filters
+#from Fbot import fbot
+#from config import Config
 
-a = [594813047] # add user ids as list
-#b = -1001142186094 # add the chat id of channel
-CHANNEL_ID = Config.CHANNEL_ID
+#a = [594813047] # add user ids as list
+#b = -1001142186094 # add the chat id
 
-@fbot.on_message(filters.chat(a))
-async def fwd(fbot, message):
-    await message.forward(Config.CHANNEL)
+#@fbot.on_message(filters.chat(a))
+#async def fwd(fbot, message):
+    #await message.forward(b)
