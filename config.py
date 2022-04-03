c9bc42e7fbcd2f9851518ae6a4cb01bece2dce4c
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-04-03 11:41:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config(object):
       TOKEN = os.environ.get("BOT_TOKEN", "")
       APP_ID = int(os.environ.get("APP_ID", 12345))
       API_HASH = os.environ.get("API_HASH")
-      CHANNEL = list(x for x in os.environ.get("CHANNEL_ID", "").replace("\n", " ").split(' '))
+      CHANNEL_ID = list(x for x in os.environ.get("CHANNEL_ID", "").replace("\n", " ").split(' '))
 
 AUTH_USERS = set()
 
