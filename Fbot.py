99bae929e3679b204b4156dbcaf5968984be3934
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-04-05 09:31:00 +0530

    Update Fbot.py

--- Fbot.py
+++ Fbot.py
@@ -1,32 +1,32 @@
+import os
 import pyrogram
-
 import logging
+
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
-
-import os
 
 from config import Config
 from pyrogram import Client, __version__
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-class fbot(Client):
-    
-    def __init__(self):
-        super().__init__(
-            session_name="FBOT",
-            bot_token = Config.TOKEN,
-            api_id = Config.APP_ID,
-            api_hash = Config.API_HASH,
-            workers = 20,
-            plugins = dict(
-                root="plugins"
-            )
-        )
 
 if __name__ == "__main__" :
-    fbot().run()
+    # create download directory, if not exist
+    if not os.path.isdir(Config.DOWNLOAD_LOCATION):
+        os.makedirs(Config.DOWNLOAD_LOCATION)
+    plugins = dict(
+        root="plugins"
+    )
+    fbot = pyrogram.Client(
+        "Fbot",
+        bot_token=Config.TOKEN,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        plugins=plugins
+    )
+    Config.AUTH_USERS.add(594813047)
+    fbot.run()
     logger.info(
             f"Pyrogram v{__version__} "
             "Hi."
