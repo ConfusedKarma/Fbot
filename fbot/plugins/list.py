d7a1b1f169f3ca225e5e472144639347513e0498
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-04-05 12:02:37 +0530

    Update list.py

--- fbot/plugins/list.py
+++ fbot/plugins/list.py
@@ -32,8 +32,7 @@ def bot_list(fbot, message):
     if chat_type != "private":
         bots = ""
         for i in fbot.get_chat_members(message.chat.id, filter="bots"):
-            bots += " 🤖 -> @{}\n".format(i.user.username) if i.user.username \
-                        else " ⛑ -> [{}](tg://user?id={})\n".format(
+            bots += " 🤖 -> @{}\n".[{}](tg://user?id={})\n".format(
                             i.user.first_name, i.user.id
                         )
         message.reply(f'Bot list:\n{bots}', parse_mode="Markdown", disable_web_page_preview=True)
