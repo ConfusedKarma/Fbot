c370fd0065fc56b0fdb566ba3d7edd225a913a32
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-04-05 09:45:49 +0530

    Create sample_config.py

---
+++ fbot/sample_config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv("config.env")
+
+
+class Config(object):
+      LOGGER = True
+      TOKEN = os.environ.get("BOT_TOKEN", "")
+      APP_ID = int(os.environ.get("APP_ID", 12345))
+      API_HASH = os.environ.get("API_HASH")
+      CHANNEL_ID = list(x for x in os.environ.get("CHANNEL_ID", "").replace("\n", " ").split(' '))
+      # Array to store users who are authorized to use the bot
+      AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+      #download location
+      DOWNLOAD_LOCATION = "./DOWNLOADS"
