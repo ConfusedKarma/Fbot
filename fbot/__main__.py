9290d7f0aa87d4675ccddf9d019cdc80598637e6
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-04-05 11:38:18 +0530

    Update __main__.py

--- fbot/__main__.py
+++ fbot/__main__.py
@@ -1,10 +1,10 @@
+import logging
 from .fbot import fbot
-from fbot import LOGGER
 from pyrogram import __version__
 
 if __name__ == "__main__":
     fbot().run()
-    LOGGER.info(
+    logging.info(
             f"BOT RUNNING!"
             f"Pyrogram version - {__version__} "
         )
