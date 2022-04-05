1d6a16ae80d95caceb3bfc3e67585b59f7091c80
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-04-05 10:01:42 +0530

    Create __init__.py

---
+++ fbot/__init__.py
@@ -0,0 +1,25 @@
+# needed to appropriately, select ENV vars / Config vars
+import os
+
+# the logging things
+import logging
+
+from pyrobot.sample_config import Config
+
+
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+LOGGER = logging.getLogger(__name__)
+APP_ID = Config.APP_ID
+API_HASH = Config.API_HASH
+BOT_TOKEN = Config.BOT_TOKEN
+CUSTOM_CMD = Config.CUSTOM_CMD
+AUTH_USERS = Config.AUTH_USERS
+DOWNLOAD_LOCATION = Config.DOWNLOAD_LOCATION
+# create download directory, if not exist
+if not os.path.isdir(DOWNLOAD_LOCATION):
+    os.makedirs(DOWNLOAD_LOCATION)
