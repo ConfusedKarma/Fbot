500a6ad22840f3b85e7927600a86ba07de1ef2a8
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-04-05 12:25:12 +0530

    Create nekobin.py

---
+++ fbot/plugins/nekobin.py
@@ -0,0 +1,23 @@
+from consts import http
+from fbot import CUSTOM_CMD
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+
+@Client.on_message(filters.command("paste", CUSTOM_CMD))
+async def nekobin(c: Client, m: Message):
+    if m.reply_to_message:
+        if m.reply_to_message.document:
+            tfile = m.reply_to_message
+            to_file = await tfile.download()
+            with open(to_file, "rb") as fd:
+                mean = fd.read().decode("UTF-8")
+        if m.reply_to_message.text:
+            mean = m.reply_to_message.text
+
+        url = "https://nekobin.com/api/documents"
+        r = await http.post(url, json={"content": mean})
+        url = f"https://nekobin.com/{r.json()['result']['key']}"
+        await m.reply_text(url, disable_web_page_preview=True)
+    else:
+        await m.reply_text("Reply to Document or Text File")
