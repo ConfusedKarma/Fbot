81c4b7b3b9d9406b7b8e7b449fbd76badfa65cb6
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-04-05 09:16:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,3 +7,5 @@ class Config(object):
       CHANNEL_ID = list(x for x in os.environ.get("CHANNEL_ID", "").replace("\n", " ").split(' '))
       # Array to store users who are authorized to use the bot
       AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+      #download location
+      DOWNLOAD_LOCATION = "./DOWNLOADS"
