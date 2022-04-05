a0bbe0471c526b9da85c247410241718203421e1
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-04-05 09:58:12 +0530

    Update sample_config.py

--- fbot/sample_config.py
+++ fbot/sample_config.py
@@ -4,8 +4,9 @@ class Config(object):
       TOKEN = os.environ.get("BOT_TOKEN", "")
       APP_ID = int(os.environ.get("APP_ID", 12345))
       API_HASH = os.environ.get("API_HASH")
+      CUSTOM_CMD = os.environ.get("CUSTOM_CMD", "!")
       CHANNEL_ID = list(x for x in os.environ.get("CHANNEL_ID", "").replace("\n", " ").split(' '))
       # Array to store users who are authorized to use the bot
-      AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+      AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", 594813047).split())
       #download location
       DOWNLOAD_LOCATION = "./DOWNLOADS"
