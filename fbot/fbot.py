e683cac009833ceb5460ff820113d021b8f1f036
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-04-05 10:17:19 +0530

    Create fbot.py

---
+++ fbot/fbot.py
@@ -0,0 +1,23 @@
+from typing import dict
+
+from fbot import (
+    APP_ID,
+    API_HASH,
+    BOT_TOKEN,
+    DOWNLOAD_LOCATION
+)
+
+class fbot(Client):
+
+    def __init__(self):
+        name = self.__class__.__name__.lower()
+        super().__init__(
+            ":memory:",
+            plugins=dict(root=f"{name}/plugins"),
+            workdir=DOWNLOAD_LOCATION,
+            api_id=APP_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            parse_mode="html",
+            sleep_threshold=60,
+        )
