31b56fdc622eae836ada9eeb58fbff7419c30251
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-04-07 18:33:06 +0530

    Update eval.py

--- fbot/plugins/eval.py
+++ fbot/plugins/eval.py
@@ -8,13 +8,13 @@ import io
 import sys
 import traceback
 from pyrogram import Client, filters
-from fbot import CUSTOM_CMD
+from fbot import CUSTOM_CMD, fbot
 
 
 MAX_MESSAGE_LENGTH = 4096
 
 @Client.on_message(filters.command("eval", CUSTOM_CMD))
-async def eval(client, message):
+async def eval(fbot, client, message):
     status_message = await message.reply_text("Processing ...")
     cmd = message.text.split(" ", maxsplit=1)[1]
 
