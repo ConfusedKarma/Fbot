5146611fd7bca473dba0400ab4fe7abb6cdb5f68
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-04-12 09:08:46 +0530

    Update fwd.py

--- fbot/plugins/fwd.py
+++ fbot/plugins/fwd.py
@@ -1,6 +1,5 @@
-#from pyrogram import filters
-#from fbot import fbot
-#from config import Config
+from pyrogram import filters
+from fbot import fbot, AUTH_USERS, TO_CHANNEL
 
 #a = [594813047] # add user ids as list
 #b = -1001142186094 # add the chat id
@@ -8,3 +7,33 @@
 #@fbot.on_message(filters.chat(a))
 #async def fwd(fbot, message):
     #await message.forward(b)
+
+
+
+
+@Fbot.on_message(
+    filters.chat & (
+        filters.text
+        filters.audio
+        filters.document
+        filters.photo
+        filters.sticker
+        filters.video
+        filters.animation
+        filters.voice
+        filters.video_note
+        filters.contact
+        filters.location
+        filters.venue
+        filters.poll
+        filters.game
+    )
+)
+async def autopost(bot, update):
+    if len(AUTH_USERS) == 0 or len(TO_CHANNEL) == 0 or update.chat.id not in AUTH_USERS:
+        return
+    try:
+        for chat_id in TO_CHANNEL:
+                await update.forward(chat_id=chat_id)
+    except Exception as error:
+        print(error)
