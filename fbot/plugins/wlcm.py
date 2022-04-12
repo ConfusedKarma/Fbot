406a8a77ceedf52c250e4fbeec130948865857ec
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-04-12 21:46:34 +0530

    Create wlcm.py

---
+++ fbot/plugins/wlcm.py
@@ -0,0 +1,16 @@
+import asyncio
+from asyncio import sleep
+from pyrogram import Client, filters
+from fbot.sample_config import Config
+from fbot import TO_CHANNEL
+
+WELCOME_CHANNEL = Config.TO_CHANNEL
+
+@Client.on_message(filters.chat(WELCOME_CHANNEL) & filters.new_chat_members)
+async def welcome(client, message):
+    if message.from_user.is_bot:
+        await chat.kick_member(message.from_user.id)
+    else:
+        await message.reply_text(" Hi %(message.from_user.first_name)s , 🎉 Welcome to chat! 🎉", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="🔗  Join @ChuPeeps  🔗",url="t.me/ChuPeeps")],[InlineKeyboardButton(text="🚩  Bot Owner  🚩",url ="t.me/refundisillegal")]]))
+        await asyncio.sleep(60)
+        await message.delete()
