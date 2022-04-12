84ca8f327b3a271b68d165bf431824599e28f710
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-04-12 08:59:38 +0530

    Update sample_config.py

--- fbot/sample_config.py
+++ fbot/sample_config.py
@@ -9,7 +9,7 @@ class Config(object):
       APP_ID = int(os.environ.get("APP_ID", 12345))
       API_HASH = os.environ.get("API_HASH")
       CUSTOM_CMD = os.environ.get("CUSTOM_CMD", "!")
-      CHANNEL_ID = list(x for x in os.environ.get("CHANNEL_ID", "").replace("\n", " ").split(' '))
+      TO_CHANNEL = list(x for x in os.environ.get("TO_CHANNEL", "").replace("\n", " ").split(' '))
       # Array to store users who are authorized to use the bot
       AUTH_USERS = list(
   set(
