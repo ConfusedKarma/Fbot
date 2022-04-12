e9bed6857c5ed09d65b38bdf866ac614404308cc
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-04-12 23:32:35 +0530

    Update wlcm.py

--- fbot/plugins/wlcm.py
+++ fbot/plugins/wlcm.py
@@ -5,12 +5,21 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 WELCOME_CHANNEL = -1001193710741
 
+JOIN_TEXT = """Hello {} Welcome to ChuPeeps"""
+
+BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('Owner', url='https://telegram.me/refundisillegal')],[InlineKeyboardButton('Owner', url='t.me/ChuPeeps]])
+
 
 @Client.on_message(filters.chat(WELCOME_CHANNEL) & filters.new_chat_members)
 async def welcome(client, message):
     if message.from_user.is_bot:
         await chat.kick_member(message.from_user.id)
     else:
-        await message.reply_text(" Hello {}, 🎉 Welcome to chat! 🎉", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="🔗  Join @ChuPeeps  🔗",url="t.me/ChuPeeps")],[InlineKeyboardButton(text="🚩  Bot Owner  🚩",url ="t.me/refundisillegal")]])).format(update.from_user.mention)
+        await message.reply_text(
+        text=JOIN_TEXT.format(update.from_user.mention),
+        reply_markup=BUTTONS,
+        disable_web_page_preview=True,
+        quote=True
+    )
         await asyncio.sleep(60)
         await message.delete()
