026135baba4be0a8d1c376494d195f3869d136ee
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-04-18 22:23:32 +0530

    Update admin.py

--- fbot/plugins/admin.py
+++ fbot/plugins/admin.py
@@ -33,7 +33,7 @@ async def admin_check(message: Message) -> bool:
 mute_permission = ChatPermissions(
     can_send_messages=False,
     can_send_media_messages=False,
-    can_send_stickers=False,
+    can_send_other_messages=False,
     can_send_animations=False,
     can_send_games=False,
     can_use_inline_bots=False,
@@ -48,7 +48,7 @@ mute_permission = ChatPermissions(
 unmute_permissions = ChatPermissions(
     can_send_messages=True,
     can_send_media_messages=True,
-    can_send_stickers=True,
+    can_send_other_messages=True,
     can_send_animations=True,
     can_send_games=True,
     can_use_inline_bots=True,
