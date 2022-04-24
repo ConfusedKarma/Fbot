8157f1d507cebe4531e76b6cac2c73837dee0d2f
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-04-24 14:13:34 +0530

    Create telegraph.py

---
+++ fbot/plugins/telegraph.py
@@ -0,0 +1,20 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from telegraph import upload_file
+from fbot import CUSTOM_CMD
+
+
+@Client.on_message(filters.command("tlg", CUSTOM_CMD) & ~filters.edited)
+async def telegraph(c: Client, m: Message, strings):
+    if m.reply_to_message:
+        if (
+            m.reply_to_message.photo
+            or m.reply_to_message.video
+            or m.reply_to_message.animation
+        ):
+            d_file = await m.reply_to_message.download()
+            media_urls = upload_file(d_file)
+            tele_link = "https://telegra.ph" + media_urls[0]
+            await m.reply_text(tele_link)
+    else:
+        await m.reply_text("Reply?"))
