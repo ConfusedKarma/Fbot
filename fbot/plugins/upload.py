5794507d1778faec65f64b73e69b34978149a9ca
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-05-08 19:16:33 +0530

    Update upload.py

--- fbot/plugins/upload.py
+++ fbot/plugins/upload.py
@@ -18,7 +18,7 @@ async def upload(bot, message: Message):
     if len(message.command) > 1:
         await bot.send_document('message.chat.id', message.command[1], progress=progress_callback, progress_args=(bot, message))
     else:
-        await message.edit('No path provided.')
+        await message.reply('No path provided.')
         await asyncio.sleep(3)
 
     await message.delete()
