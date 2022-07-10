137bc14361db4f98634882fe61a03693f7265569
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-07-10 08:31:48 +0530

    Update bypass.py

--- fbot/plugins/bypass.py
+++ fbot/plugins/bypass.py
@@ -4,10 +4,9 @@ import cloudscraper
 from pyrogram import Client, filters
 from fbot import CUSTOM_CMD, AUTH_USERS
 from fbot.sample_config import Config
-import time
-import requests
-from bs4 import BeautifulSoup
+import asyncio
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup
 
 
 
@@ -47,34 +46,48 @@ async def mdik(bot, update):
         text=bsdk, disable_web_page_preview=True, quote=True
     )
 
-def gp(url):
-    scraper = cloudscraper.create_scraper(allow_brotli=False)
-    src = scraper.get(url)
-    header = { "referer": src.url }
-    src = scraper.get(url, headers=header)
-    
-    bs4 = BeautifulSoup(src.content, 'html.parser')
+async def get_link(url: str):
+    client = cloudscraper.create_scraper(allow_brotli=False)
+    p = urlparse(url)
+    final_url = f'{p.scheme}://{p.netloc}/links/go'
+
+    res = client.head(url)
+    header_loc = res.headers['location']
+    param = header_loc.split('postid=')[-1]
+    req_url = f'{p.scheme}://{p.netloc}/{param}'
+
+    p = urlparse(header_loc)
+    ref_url = f'{p.scheme}://{p.netloc}/'
+
+    h = { 'referer': ref_url }
+    res = client.get(req_url, headers=h, allow_redirects=False)
+
+    bs4 = BeautifulSoup(res.content, 'html.parser')
     inputs = bs4.find_all('input')
     data = { input.get('name'): input.get('value') for input in inputs }
 
-    header = {
-        'content-type': 'application/x-www-form-urlencoded',
-        'x-requested-with': 'XMLHttpRequest'
+    h = {
+        'referer': ref_url,
+        'x-requested-with': 'XMLHttpRequest',
     }
+    asyncio.sleep(10)
+    res = client.post(final_url, headers=h, data=data)
+    try:
+        return res.json()['url'].replace('\/','/')
+    except Exception as e:
+        print(e)
+        return False
+
+
+async def gp(message):
+    if not message.text.startswith("https://gplinks") or message.text.startswith("gplinks"):
+       await message.reply_text("Sorry, all I do is scrape GPLinks URLs :(")
+       return
+    m = await message.reply_text("Please wait...")
+    link = await get_link(message.text)
+    await m.delete()
+    if not link:      
+       await message.reply_text("Something went wrong\nTry again later..")
+    else:
+       await message.reply_text(f"Here is your direct link:\n\n{link}", disable_web_page_preview=True)
     
-    time.sleep(5) 
-    
-    k = urlparse(url)
-    URL = f'{k.scheme}://{k.netloc}/links/go'
-    src = scraper.post(URL, data=data, headers=header).json()
-
-    return src
-
-@Client.on_message(filters.command("gp", CUSTOM_CMD) & filters.user(Config.AUTH_USERS))
-async def gplink(_, message): 
-
-    link = message.command[1]
-
-    gpLink = gp(url=link)
-    
-    await message.reply_text(f"{gpLink}")
