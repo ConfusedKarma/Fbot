c138fa4876d073dec0793e266e84eb71ad5db80b
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-07-15 11:04:47 +0530

    Update hastebin.py

--- fbot/plugins/hastebin.py
+++ fbot/plugins/hastebin.py
@@ -30,3 +30,27 @@ async def hastebin(c: Client, m: Message):
         await m.reply_text("[HASTEBIN]({}) in\n{} seconds".format(url, ms, disable_web_page_preview=True))
     else:
         await m.reply_text("Reply to Document or Text File")
+
+
+from requests import post, get
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+def paste(text):
+    url = "https://spaceb.in/api/v1/documents/"
+    res = post(url, data={"content": text, "extension": "txt"})
+    return f"https://spaceb.in/{res.json()['payload']['id']}"
+
+
+@Client.on_message(filters.command("pst", CUSTOM_CMD) & filters.user(Config.AUTH_USERS) & ~filters.edited)
+def pastex(_, message):
+    text = message.reply_to_message
+    if text:
+        x = paste(text.text)
+        message.reply(x,
+                      reply_markup=InlineKeyboardMarkup(
+                          [[InlineKeyboardButton("Open", url=x)]]),
+                      disable_web_page_preview=True)
+
+    else:
+        message.reply_text("Reply to a message!")
